d1db1cacdabd218723b336d8c39736fd1300d7fc
Juli4Ros4 <rosacosmejulia@gmail.com>
2025-04-25 19:42:52 -0300

    first commit

---
+++ mysite/urls.py
@@ -0,0 +1,8 @@
+
+from django.contrib import admin # type: ignore
+from django.urls import path , include # type: ignore
+
+urlpatterns = [
+    path("polls/", include("polls.urls")),
+    path('admin/', admin.site.urls),
+]
